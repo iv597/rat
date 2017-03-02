1b3a45fbb35db92896074f8e24934952b0e49311
Josh Klar <j@iv597.com>
2017-03-01 23:55:01 -0600

    Had to fork&fix, but ICS files can be read now

--- src/client/importcli/mod.rs
+++ src/client/importcli/mod.rs
@@ -7,7 +7,6 @@ use std::io::BufReader;
 use std::fs::File;
 
 pub fn entry(m: &SubCommand) -> i32 {
-	println!("{:?}", m);
 	let fname = m.matches.value_of("filename").unwrap();
 
 	let file = match File::open(fname) {
@@ -21,7 +20,15 @@ pub fn entry(m: &SubCommand) -> i32 {
 	let input = IcalParser::new(BufReader::new(file));
 
 	for line in input {
-		println!("{:?}", line);
+		let line = line.unwrap();
+
+		for evt in line.events {
+			println!("EVENT");
+
+			for prop in evt.properties {
+				println!("\t{:?}", prop);
+			}
+		}
 	}
 
 	0
