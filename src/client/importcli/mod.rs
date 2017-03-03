4b530b0a1275431ae3774372d4c94a3023b36153
Josh Klar <j@iv597.com>
2017-03-02 20:57:54 -0600

    Busted compile due to possible upstream bug? But otherwise maybe sane?

--- src/client/importcli/mod.rs
+++ src/client/importcli/mod.rs
@@ -1,10 +1,26 @@
+extern crate chrono;
+extern crate chrono_tz;
 extern crate clap;
 extern crate ical;
 
+use std::fs::File;
+use std::io::BufReader;
+use self::chrono::{DateTime, TimeZone};
+use self::chrono_tz::{Tz, UTC};
 use self::clap::SubCommand;
 use self::ical::IcalParser;
-use std::io::BufReader;
-use std::fs::File;
+
+#[derive(Debug)]
+enum LineInfo {
+	TextInfo {
+		name: String,
+		value: Option<String>,
+	},
+	DateTimeInfo {
+		name: String,
+		value: DateTime<UTC>,
+	}
+}
 
 pub fn entry(m: &SubCommand) -> i32 {
 	let fname = m.matches.value_of("filename").unwrap();
@@ -26,7 +42,39 @@ pub fn entry(m: &SubCommand) -> i32 {
 			println!("EVENT");
 
 			for prop in evt.properties {
-				println!("\t{:?}", prop);
+				let parsedval: LineInfo = match prop.value {
+					Some(mut v) => {
+						let tz = if (&v).ends_with("Z") {
+							v.pop();
+
+							UTC
+						} else {
+							match prop.params {
+								Some(params) => {
+									for &(ref pk, ref pv) in &params {
+										if pk == "TZID" {
+											match pv[0].parse::<Tz>() {
+												Ok(tz) => tz,
+												Err(_) => UTC,
+											}
+										}
+									}
+
+									UTC
+								}
+								None => UTC,
+							}
+						};
+
+						match tz.datetime_from_str(&v, "%Y%m%dT%H%M%S") {
+							Ok(v) => LineInfo::DateTimeInfo { name: prop.name, value: v },
+							Err(_) => LineInfo::TextInfo { name: prop.name, value: Some(v) },
+						}
+					}
+					None => LineInfo::TextInfo { name: prop.name, value: None },
+				};
+
+				println!("{:?}", parsedval);
 			}
 		}
 	}
